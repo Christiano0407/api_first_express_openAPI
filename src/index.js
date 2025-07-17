fd53553df9b39cca5297380fab68d64437b894a8
christiano0407 <placebo0704@gmail.com>
2025-07-17 09:14:34 -0600

    [apiFirst/openAPI] Add: New files and change package json & yaml

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const express = require('express'); 
+const app = express(); 
+const port = 3000;
+
+app.get('/hello', (req, res) => {res.json({message: 'Hello World with OpenAPI 3.1.1 | API First'})}); 
+
+app.listen(port, () => {console.log(`Server is running in port ${port} | Production server`)}); 
