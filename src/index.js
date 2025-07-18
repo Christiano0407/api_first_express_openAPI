c32f7a35d1de1e83af5db1818b25cae720d73fb3
christiano0407 <placebo0704@gmail.com>
2025-07-18 09:29:18 -0600

    [apiFirst/openAPI] Add: OpenAPIValidators

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import YAML from "js-yaml";
 import path from "path";
 import { fileURLToPath } from "url";
 import { readFileSync } from "fs";
+import pkg from "express-openapi-validator";
+const { OpenApiValidator } = pkg; // Open API Validator | Come Common Js
 
 const app = express();
 const port = 3000;
@@ -16,12 +18,39 @@ const swaggerDocument = YAML.load(
 
 app.use(`/api-docs`, SwaggerUI.serve, SwaggerUI.setup(swaggerDocument));
 
+const apiSpecPath = path.join(__dirname, `../openAPI/api.yaml`); 
+
+app.use(
+  OpenApiValidator.middleware({
+    apiSpec: apiSpecPath,
+    validateRequests: true, // Validate request: body, query, params
+    validateResponses: true, // Validate Response
+  }),
+);
+
 app.get(`/hello`, (req, res) => {
   res
     .status(200)
     .json({ message: `Hello World with OpenAPI 3.1.1 | API First` });
 });
 
+// = Middleware To Handle Validation Errors =
+app.use((err, req, res, next) => {
+  if (err.status & err.errors) {
+    res.status(err.status).json({
+      message: err.message,
+      errors: err.errors,
+    });
+  } else {
+    res.status(500).json({
+      message: `Internal Server Error`,
+      errors: err.message || `Something went wrong`,
+    });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is running in port ${port} | Production server`);
+  console.log(`OpenAPI Documentation is Available ar /api-docs`);
+  console.log(`Open API Specification is Available at ${apiSpecPath}`); 
 });
