7896864020ce96b0333887604853050e991a8e4a
christiano0407 <placebo0704@gmail.com>
2025-07-30 09:41:59 -0600

    [apiFirst/openAPI] Add: Logic To database

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,3 +1,13 @@
 // === Logic Develop DataBase / Logic Business ===
 
 import pool from "../db.js"; // = Package DB - pg =  
+
+export const getAllProducts = async(req, res) => {
+  try {
+    const result = await pool.query(`SELECT * FROM producto ORDER BY id_producto ASC`); 
+    res.json(result.rows); 
+  }catch(err) {
+    console.error(`Error To Get all Products: ${err}`); 
+    res.status(500).json({error: `Internal Error Server: 500`}); 
+  }
+}; 
