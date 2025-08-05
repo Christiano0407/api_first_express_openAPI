baba4185f7034b9cc34abeaf80e0129aa686e6a2
christiano0407 <placebo0704@gmail.com>
2025-08-05 12:59:04 -0600

    [apiFirst/openAPI] Tests % Mocks in Jest In Process...

--- __tests__/unit/controllers/productController.test.js
+++ __tests__/unit/controllers/productController.test.js
@@ -1,1 +1,36 @@
 // ====== === TESTS: TESTS Unitarios - Controllers Data Validation === ======
+import * as productsControllers from "../../../src/controllers/productsController.js"; 
+import { testPool, setupTestCreateDb, cleanTestDb, closeTestDb } from "../setup/testDB.js";
+import { createProductMock  } from "../setup/testHelpers.js"; 
+import { beforeEach, describe, jest } from "@jest/globals";
+import pool from "../../../src/db";
+
+// === Mocks (Jest | No Prims) | Replace our Original Data in PostgreSQL ===
+// = Default is 'default export' = 
+jest.mock("../../../src/db.js", () => {
+  default: {
+    query: jest.fn()
+  }
+});  
+
+describe("Products Controllers - Unit Test", () => {
+  let mockReq, mockRes; 
+
+  beforeEach(() => {
+    mockReq = {
+      params: {}, 
+      query: {}, 
+      body: {}
+    }; 
+
+    mockRes = {
+      json: jest.fn().mockReturnThis(), 
+      status: jest.fn().mockReturnThis()
+    }; 
+
+    jest.clearAllMocks(); 
+  });
+
+}); 
+
+// === Tests Controllers ===
